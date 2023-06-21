e77fd803ee3968ee2184908d35f7884e44fbd3d1
Bob Park <hwpark11@kakao.com>
2023-06-21 23:32:16 +0900

    build: axois dependency 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.CLIENT_SERVICE_PATH + '/:path*',
+      },
+    ];
+  },
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
