ce57213fb049b61ea6d91dbc651b49f478687837
Bob Park <hwpark11@kakao.com>
2023-07-03 23:29:53 +0900

    feat: 사용자 정보 UI 추가

--- src/pages/settings/profile.tsx
+++ src/pages/settings/profile.tsx
@@ -1,11 +1,14 @@
 // react
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 // next
 import { useRouter } from 'next/router';
 
 // daisyui
-import { Button, Modal, Input } from 'react-daisyui';
+import { Button, Modal, Input, Divider } from 'react-daisyui';
+
+// react-icons
+import { GrEdit } from 'react-icons/gr';
 
 // hooks
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHook';
@@ -24,15 +27,86 @@ export default function Profile() {
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.user);
 
+  // state
+
+  // ref
+  const avatarInputRef = useRef<HTMLInputElement>(null);
+
+  // handle
+  const handleEditAvatar = () => {
+    avatarInputRef.current?.click();
+  };
+
+  const handleAvatarChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (!files) {
+      return;
+    }
+
+    const avatarFile = files[0];
+
+    // ? 확장자...확인 필요할까?
+
+    const formData = new FormData();
+
+    formData.append('avatar', avatarFile);
+  };
+
   return (
     <>
       <main className="grid grid-cols-3 gap-10 w-full h-full p-10 m-5">
         {/* title */}
         <div className="col-span-3">
-          <h1>프로필</h1>
+          <h1 className="text-xl font-semibold">프로필</h1>
+          <Divider />
         </div>
-        <div className="col-span-2">name</div>
-        <div className="col-span-1">profile</div>
+        <div className="col-span-2">
+          <div className="grid grid-cols-1 gap-10">
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">팀</span>
+              </label>
+              <Input value={user?.team.name} disabled />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">직급</span>
+              </label>
+              <Input value={user?.position?.name} disabled />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">이름</span>
+              </label>
+              <Input value={user?.name} disabled />
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="w-[256px] relative">
+            <img
+              className="w-[256px] h-[256px] border rounded-full"
+              src={user?.avatar}
+            />
+            <input
+              type="file"
+              hidden
+              ref={avatarInputRef}
+              accept=".png,.jpg"
+              onChange={handleAvatarChange}
+            />
+            <Button
+              className="absolute bottom-0 border border-solid border-gray-300"
+              animation
+              color="ghost"
+              onClick={handleEditAvatar}
+            >
+              <GrEdit />
+              Edit
+            </Button>
+          </div>
+        </div>
       </main>
     </>
   );
